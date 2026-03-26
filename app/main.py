081914b97fb01bbc8e65828226cc90bd97ba5022
John Merrick Monteclaro <merrick.monteclaro@gmail.com>
2026-03-26 14:34:35 +0800

    Solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 class Person:
     people = {}
-    def __init__(self, name, age):
+
+    def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
         Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    person_list = [Person(p["name"], p["age"]) for p in people]
-    for p in people:
-        inst = Person.people[p["name"]]
-        wife_name = p.get("wife")
-        husband_name = p.get("husband")
+    person_list = [Person(peeps["name"], peeps["age"]) for peeps in people]
+    for peeps in people:
+        inst = Person.people[peeps["name"]]
+        wife_name = peeps.get("wife")
+        husband_name = peeps.get("husband")
         if wife_name:
             inst.wife = Person.people[wife_name]
         if husband_name:
